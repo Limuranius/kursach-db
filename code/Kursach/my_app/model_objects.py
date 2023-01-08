2723080b79406faf680ce3b3157b993be7e85a32
Limuran <strelgl2@gmail.com>
2023-01-08 18:18:28 +1000

    Перенесены методы удаления и редактирования с моделей на объекты модели

--- code/Kursach/my_app/model_objects.py
+++ code/Kursach/my_app/model_objects.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 import datetime
 from enum import Enum
+import mysql.connector
+from mysql.connector import CMySQLConnection
+from mysql.connector.connection_cext import CMySQLCursor
 
 
 class UserStatus(Enum):
@@ -12,11 +15,111 @@ class UserStatus(Enum):
     ANONYMOUS = None
 
 
+class Connection:
+    connection: CMySQLConnection = None
+
+    @classmethod
+    def get_cursor(cls) -> CMySQLCursor:
+        if cls.connection is None:
+            cls.connection = mysql.connector.connect(
+                user="root",
+                password="aboba",
+                host="database",
+                port="3306",
+                database="kursach"
+            )
+        return cls.connection.cursor()
+
+
+class BaseModelObj:
+    primary_field: str  # Определить, если одна таблица с одним главным ключом
+    table_name: str  # Определить, если одна таблица
+    mysql = Connection.get_cursor()
+
+    def update(self, *new_params):
+        # Метод для объектов с одной таблицей с одним главным ключом.
+        # Если это не так, то переопределить
+        set_fields = ",".join([f"{field} = %s" for field in self.get_manual_fields()])
+        query = f"""
+            UPDATE {self.table_name}
+            SET {set_fields}
+            WHERE {self.primary_field} = %s
+        """
+        primary_value = getattr(self, self.primary_field)
+        self.mysql.execute(query, (*new_params, primary_value))
+
+    def remove(self):
+        # Метод для объектов с одной таблицей с одним главным ключом.
+        # Если это не так, то переопределить
+        query = f"""
+            DELETE FROM {self.table_name}
+            WHERE {self.primary_field} = %s
+        """
+        primary_value = getattr(self, self.primary_field)
+        self.mysql.execute(query, primary_value)
+
+    def __iter__(self):
+        pass
+
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        pass
+
+
 @dataclass
-class ProviderObj:
+class ProviderObj(BaseModelObj):
     provider_id: int
     name: str
     address: str
+
+    def update(self, name: str, address: str):
+        self.__update_name(name)
+        self.__update_address(address)
+
+    def __update_name(self, name: str):
+        self.mysql.execute("""
+            UPDATE provider_name
+            SET name = %s
+            WHERE provider_id = %s
+        """, (name, self.provider_id))
+
+    def __update_address(self, address: str):
+        self.mysql.execute("""
+            UPDATE provider_address
+            SET address = %s
+            WHERE provider_id = %s AND address = %s
+        """, (address, self.provider_id, self.address))
+
+    def remove(self):
+        if self.__count_address() == 1:
+            self.__remove_name()  # Если удаляем последний адрес, то удаляем также и всего поставщика
+        else:
+            self.__remove_address()
+
+    def __count_address(self) -> int:
+        self.mysql.execute("""
+            SELECT COUNT(*) 
+            FROM provider_address 
+            WHERE provider_id = %s
+        """, (self.provider_id,))
+        count = self.mysql.fetchone()[0]
+        return count
+
+    def __remove_name(self):
+        self.mysql.execute("""
+            DELETE FROM provider_name
+            WHERE provider_id = %s
+        """, (self.provider_id,))
+
+    def __remove_address(self):
+        self.mysql.execute("""
+            DELETE FROM provider_address
+            WHERE provider_id = %s AND address = %s
+        """, (self.provider_id, self.address))
+
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["name", "address"]
 
     def __iter__(self):
         return iter([
@@ -27,11 +130,17 @@ class ProviderObj:
 
 
 @dataclass
-class FlowerObj:
+class FlowerObj(BaseModelObj):
+    primary_field = "flower_id"
+    table_name = "flower"
+
     flower_id: int
     name: str
     price: int
     provider_id: int
+
+    def update(self, name: str, price: int, provider_id: int):
+        super().update(name, price, provider_id)
 
     def __iter__(self):
         return iter([
@@ -41,13 +150,61 @@ class FlowerObj:
             self.provider_id
         ])
 
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["name", "price", "provider_id"]
+
 
 @dataclass
-class CustomerObj:
+class CustomerObj(BaseModelObj):
     customer_id: int
     name: str
     phone: str
     address: str
+
+    def update(self, name, phone, address):
+        self.__update_name(name)
+        self.__update_info(phone, address)
+
+    def __update_name(self, name: str):
+        self.mysql.execute("""
+            UPDATE customer_name
+            SET name = %s
+            WHERE customer_id = %s
+        """, (name, self.customer_id))
+
+    def __update_info(self, phone: str, address: str):
+        self.mysql.execute("""
+            UPDATE customer_info
+            SET phone = %s AND address = %s
+            WHERE customer_id = %s AND phone = %s
+        """, (phone, address, self.customer_id, self.phone))
+
+    def remove(self):
+        if self.__count_info() == 1:
+            self.__remove_name()  # Если удаляем последнюю информацию, то удаляем также и всего заказчика
+        else:
+            self.__remove_info()
+
+    def __count_info(self) -> int:
+        self.mysql.execute("""
+            SELECT COUNT(*)
+            FROM customer_info
+            WHERE customer_id = %s
+        """, (self.customer_id,))
+        return self.mysql.fetchone()[0]
+
+    def __remove_name(self):
+        self.mysql.execute("""
+            DELETE FROM customer_name
+            WHERE customer_id = %s
+        """, (self.customer_id,))
+
+    def __remove_info(self):
+        self.mysql.execute("""
+            DELETE FROM customer_info
+            WHERE customer_id = %s AND phone = %s
+        """, (self.customer_id, self.phone))
 
     def __iter__(self):
         return iter([
@@ -57,13 +214,23 @@ class CustomerObj:
             self.address
         ])
 
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["name", "phone", "address"]
+
 
 @dataclass
-class ContractObj:
+class ContractObj(BaseModelObj):
+    primary_field = "contract_id"
+    table_name = "contract"
+
     contract_id: int
     customer_id: int
     register_date: datetime.date
     execution_date: datetime.date
+
+    def update(self, customer_id: int, register_date: datetime.date, execution_date: datetime.date):
+        super().update(customer_id, register_date, execution_date)
 
     def __iter__(self):
         return iter([
@@ -73,28 +240,48 @@ class ContractObj:
             self.execution_date
         ])
 
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["customer_id", "register_date", "execution_date"]
+
 
 @dataclass
-class OrderObj:
-    order_id: int
+class OrderObj(BaseModelObj):
+    primary_field = "booking_id"
+    table_name = "booking"
+
+    booking_id: int
     contract_id: int
     flower_id: int
     quantity: int
 
+    def update(self, contract_id: int, flower_id: int, quantity: int):
+        super().update(contract_id, flower_id, quantity)
+
     def __iter__(self):
         return iter([
-            self.order_id,
+            self.booking_id,
             self.contract_id,
             self.flower_id,
             self.quantity
         ])
 
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["contract_id", "flower_id", "quantity"]
+
 
 @dataclass
-class EmployeeObj:
+class EmployeeObj(BaseModelObj):
+    primary_field = "login"
+    table_name = "employee"
+
     login: str
     password: str
     job_title: str
+
+    def update(self, login: str, password: str, job_title: str):
+        super().update(login, password, job_title)
 
     def __iter__(self):
         return iter([
@@ -103,12 +290,22 @@ class EmployeeObj:
             self.job_title,
         ])
 
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["login", "password", "job_title"]
+
 
 @dataclass
-class CustomerUserObj:
+class CustomerUserObj(BaseModelObj):
+    primary_field = "login"
+    table_name = "customer_user"
+
     login: str
     password: str
     customer_id: int
+
+    def update(self, login: str, password: str, customer_id: int):
+        super().update(login, password, customer_id)
 
     def __iter__(self):
         return iter([
@@ -116,3 +313,7 @@ class CustomerUserObj:
             self.password,
             self.customer_id,
         ])
+
+    @staticmethod
+    def get_manual_fields() -> list[str]:
+        return ["login", "password", "customer_id"]
